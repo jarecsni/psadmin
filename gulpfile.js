293813adad965de8535fa0210c4eae9e86dbd15c
Janos Jarecsni <janos.jarecsni@gmail.com>
2015-09-26 23:05:34 +0100

    configured the build process with lint, etc.

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,24 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect'); // run local web server
 var open = require('gulp-open'); // open a URL in the web browser
+var browserify = require('browserify'); // node modules in client side
+var reactify = require('reactify'); // JSX->JS
+var source = require('vinyl-source-stream'); // use text streams
+var concat = require('gulp-concat'); // concatenates files
+var lint = require('gulp-eslint');
 
 var config = {
     port: 9000,
     devBaseUrl: 'http://localhost',
     paths: {
-        html: './src/*.html'
+        html: './src/*.html',
+        css: [
+            'node_modules/bootstrap/dist/css/bootstrap.min.css',
+            'node_modules/bootstrap/dist/css/bootstrap-theme.min.css',
+        ],
+        js: './src/**/*.js',
+        mainJs: './src/main.js',
+        dist: './dist'
     }
 };
 
@@ -21,3 +33,43 @@ gulp.task('connect', function() {
        livereload: true
    });
 });
+
+gulp.task('open', ['connect'], function() {
+    gulp.src('dist/index.html')
+        .pipe(open({ uri: config.devBaseUrl + ':' + config.port + '/'}));
+});
+
+gulp.task('html', function() {
+    gulp.src(config.paths.html)
+        .pipe(gulp.dest(config.paths.dist))
+        .pipe(connect.reload());
+});
+
+gulp.task('js', function() {
+    browserify(config.paths.mainJs)
+        .transform(reactify)
+        .bundle()
+        .on('error', console.error.bind(console))
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest(config.paths.dist + '/scripts'))
+        .pipe(connect.reload());
+});
+
+gulp.task('css', function() {
+    gulp.src(config.paths.css)
+        .pipe(concat('bundle.css'))
+        .pipe(gulp.dest(config.paths.dist + '/css'));
+});
+
+gulp.task('lint', function() {
+    return gulp.src(config.paths.js)
+        .pipe(lint({config: 'eslint.config.json'}))
+        .pipe(lint.format());
+});
+
+gulp.task('watch', function() {
+    gulp.watch(config.paths.html, ['html']);
+    gulp.watch(config.paths.js, ['js', 'lint']);
+});
+
+gulp.task('default', ['html', 'css', 'js', 'lint', 'open', 'watch']);
---
+++ src/main.js
@@ -0,0 +1,7 @@
+/**
+ * Created by janos on 26/09/15.
+ */
+$ = jQuery = require('jquery');
+
+var App = console.log('hello world from Browserify');
+module.exports = App;
